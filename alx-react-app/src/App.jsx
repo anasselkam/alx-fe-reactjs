2e5f121e25d6a2824552e4f7655c7b9aee9eb1a4
Anass El khamlichi <anasselkhamlichi0000@gmail.com>
2025-02-23 20:39:05 +0000

    add in file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,7 +23,9 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+
       </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -24,10 +34,23 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <WelcomeMessage />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+
+      <Header />
+      <MainContent />
+      <Counter />
+      <Footer />
+
     </>
   )
 }
